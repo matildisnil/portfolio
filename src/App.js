95f0fb454f3fb095e787a898e41a3cc9d5fbf16e
Matilda Lindahl <99189688+matildisnil@users.noreply.github.com>
2022-11-22 16:20:08 +0100

    added fade effect on skillcards, not sure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Container } from '@mui/system';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
 import {
   pink, teal, cyan, blueGrey,
 } from '@mui/material/colors';
@@ -146,10 +145,10 @@ const getDesignTokens = mode => ({
 
 function App() {
   // eslint-disable-next-line no-unused-vars
-  const [mode, setMode] = React.useState('light');
+  const [mode, setMode] = React.useState('dark');
   const colorMode = React.useMemo(
     () => ({
-      // The dark mode switch would invoke this method
+      // The dark mode switch invokes this method
       toggleColorMode: () => {
         setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
       },
--- src/components/SkillsPaper.js
+++ src/components/SkillsPaper.js
@@ -1,43 +1,69 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import {
   Grid, Paper, Typography, Box,
+  Fade,
 } from '@mui/material';
 import { GoPrimitiveDot } from 'react-icons/go';
+import { useInViewport } from 'react-in-viewport';
 
-const SkillsPaper = ({ thisSkill }) => (
-  <Grid item>
-    <Box sx={{
-      padding: 1, backgroundColor: 'button3.dark', borderRadius: 1, margin: 2,
-    }}>
-      <Paper
-        elevation={2}
-        sx={{
+const SkillsPaper = ({ thisSkill }) => {
+  const myRef = useRef();
+  const [visible, setVisible] = useState(false);
 
-        }}>
+  const {
+    inViewport,
+  } = useInViewport(
+    myRef,
+    // options,
+    // config = { disconnectOnLeave: false },
+    // props,
+  );
 
-        <Box
-          className="coloredPadding"
-          sx={{
-            padding: 3, px: 4, height: 230, minWidth: 200, borderRadius: 1,
-          }}>
-          <Typography variant="subtitle" component="h3" sx={{ mb: 0.5 }}>
-            {thisSkill.title}
-          </Typography>
-          {thisSkill.techs.map(element => (
-            <Box className="skill-row" key={`${element.title}_skill`}>
-              {element.icon ? element.icon : <GoPrimitiveDot />}
-              <Typography ml={1}>
-                {' '}
-                {element.title}
-                {' '}
+  useEffect(() => {
+    if (inViewport) {
+      setTimeout(() => {
+        setVisible(true);
+      }, 100);
+    }
+  }, [inViewport]);
+
+  return (
+    <Grid item ref={myRef}>
+      <Box sx={{
+        padding: 1, backgroundColor: 'button3.dark', borderRadius: 1, margin: 2,
+      }}>
+        <Fade in={visible} timeout={1500}>
+          <Paper
+            elevation={2}
+            sx={{
+
+            }}>
+
+            <Box
+              className="coloredPadding"
+              sx={{
+                padding: 3, px: 4, height: 230, minWidth: 200, borderRadius: 1,
+              }}>
+              <Typography variant="subtitle" component="h3" sx={{ mb: 0.5 }}>
+                {thisSkill.title}
               </Typography>
+              {thisSkill.techs.map(element => (
+                <Box className="skill-row" key={`${element.title}_skill`}>
+                  {element.icon ? element.icon : <GoPrimitiveDot />}
+                  <Typography ml={1}>
+                    {' '}
+                    {element.title}
+                    {' '}
+                  </Typography>
+                </Box>
+              ))}
             </Box>
-          ))}
-        </Box>
-      </Paper>
-    </Box>
+          </Paper>
+        </Fade>
+      </Box>
 
-  </Grid>
-);
+    </Grid>
+  );
+};
 
 export default SkillsPaper;
